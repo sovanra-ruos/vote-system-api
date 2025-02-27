7bab97366d9e563d897a282082a271ee6353836e
sovanra-ruos <onsoben+1@yandex.com>
2025-02-27 22:18:34 +0700

    update docker file and docker compose

--- src/main/java/istad/co/votingsystem/VotingSystemApplication.java
+++ src/main/java/istad/co/votingsystem/VotingSystemApplication.java
@@ -1,9 +1,23 @@
 package istad.co.votingsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://vote-system-api.psa-khmer.world", description = "Deploy API Server"),
+		},
+        info = @Info(
+                title = "Vote system",
+                version = "1.0",
+                description = "Vote system"
+        )
+)
+
 public class VotingSystemApplication {
 
     public static void main(String[] args) {
